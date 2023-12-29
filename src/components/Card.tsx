3678a5983dadb48001bde38458a5c0c271559a22
kei-matsuoka <keimatsuoka0311@gmail.com>
2023-12-29 16:27:32 +0900

    Cardのサイズ修正

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,20 +4,20 @@ import { Token } from '@/types';
 
 export default function Card({ token }: { token: Token }) {
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <Link href={`tokens/${token.id}`}>
         <Image
           src={token.imageURI}
           alt={token.name}
-          width={384}
+          width={320}
           height={180}
           className="rounded-t-lg"
         />
         <div className="p-5">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
             {token.name}
           </h5>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          <p className="mb-3 font-normal text-sm text-gray-700 dark:text-gray-400">
             {token.description}
           </p>
         </div>

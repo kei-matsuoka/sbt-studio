22977559d4a17e7b77136a0494dcca8dc5dcc068
kei-matsuoka <keimatsuoka0311@gmail.com>
2023-12-29 15:59:48 +0900

    sidebarの繰り返しのコードをリファクタ

---
+++ src/constants.ts
@@ -0,0 +1,17 @@
+export const pageConfig = [
+  {
+    href: '/',
+    title: 'Home',
+    icon: '/home.svg',
+  },
+  {
+    href: '/tokens',
+    title: 'Tokens',
+    icon: '/identification.svg',
+  },
+  {
+    href: '/',
+    title: 'Members',
+    icon: '/user.svg',
+  },
+];

b6fbc6f8e6739d3055df0c21f8d14c55b61c4bde
kei-matsuoka <keimatsuoka0311@gmail.com>
2023-12-30 22:26:15 +0900

    headerとsidebarをborderからshadowへ

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 export default function Header() {
   return (
-    <header className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+    <header className="fixed top-0 z-50 w-full bg-white shadow dark:bg-gray-800">
       <div className="px-3 py-3 lg:px-5 lg:pl-3">
         <div className="flex items-center justify-between">
           <div className="flex items-center justify-start rtl:justify-end">

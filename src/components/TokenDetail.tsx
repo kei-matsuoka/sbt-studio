e7580717e6a0e1c2f5bf5171047b3de33ae53e00
kei-matsuoka <keimatsuoka0311@gmail.com>
2024-01-01 02:19:49 +0900

    svgの修正

--- src/components/TokenDetail.tsx
+++ src/components/TokenDetail.tsx
@@ -9,7 +9,13 @@ export default function TokenDetail({
   return (
     <div className="flex gap-4 mb-6">
       <div className="flex flex-col min-w-[320px] gap-2">
-        <Image src={token.imageURI} alt={token.name} width={320} height={180} className='rounded-lg' />
+        <Image
+          src={token.imageURI}
+          alt={token.name}
+          width={320}
+          height={180}
+          className="rounded-lg"
+        />
         <a
           href={`https://mumbai.polygonscan.com/address/${token.id}`}
           target="_blank"
@@ -22,14 +28,14 @@ export default function TokenDetail({
             className="w-5 h-5"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M4.25 5.5a.75.75 0 0 0-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 0 0 .75-.75v-4a.75.75 0 0 1 1.5 0v4A2.25 2.25 0 0 1 12.75 17h-8.5A2.25 2.25 0 0 1 2 14.75v-8.5A2.25 2.25 0 0 1 4.25 4h5a.75.75 0 0 1 0 1.5h-5Z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             />
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M6.194 12.753a.75.75 0 0 0 1.06.053L16.5 4.44v2.81a.75.75 0 0 0 1.5 0v-4.5a.75.75 0 0 0-.75-.75h-4.5a.75.75 0 0 0 0 1.5h2.553l-9.056 8.194a.75.75 0 0 0-.053 1.06Z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             />
           </svg>
 

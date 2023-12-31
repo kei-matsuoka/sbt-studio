bf62c64732dc77e4010eff381106f2d64b1f95a2
kei-matsuoka <keimatsuoka0311@gmail.com>
2024-01-01 02:21:28 +0900

    sidebar用の定数を修正

--- src/constants.ts
+++ src/constants.ts
@@ -10,9 +10,51 @@ export const pageConfig = [
     icon: '/identification.svg',
   },
   {
-    href: '/members',
-    title: 'Members',
-    icon: '/user.svg',
+    href: '/airdrop',
+    title: 'Airdrop',
+    icon: '/cloud.svg',
+  },
+  {
+    href: '/messaging',
+    title: 'Messaging',
+    icon: '/chat.svg',
+  },
+  {
+    href: '/analytics',
+    title: 'Analytics',
+    icon: '/chart.svg',
+  },
+  {
+    href: '/integration',
+    title: 'Integration',
+    icon: '/code.svg',
+  },
+];
+
+export const tokenConfig = [
+  {
+    id: 'erc20',
+    name: 'ERC20',
+    description:
+      'ERC20 is a technical standard used for smart contracts on the Ethereum blockchain for implementing tokens. ERC stands for Ethereum Request for Comment, and 20 is the number that was assigned to this request.',
+    imageURI: '/coin.png',
+    interfaceId: '0x36372b07',
+  },
+  {
+    id: 'erc721',
+    name: 'ERC721',
+    description:
+      'ERC721 is a free, open standard that describes how to build non-fungible or unique tokens on the Ethereum blockchain. While most tokens are fungible (every token is the same as every other token), ERC721 tokens are all unique.',
+    imageURI: '/nft.png',
+    interfaceId: '0x80ac58cd',
+  },
+  {
+    id: 'erc1155',
+    name: 'ERC1155',
+    description:
+      'ERC1155 is a standard interface for contracts that manage multiple token types. It is inspired by and inherits from ERC721, but unlike ERC721, ERC1155 tokens are fungible and minted in batches.',
+    imageURI: '/nfts.png',
+    interfaceId: '0xd9b67a26',
   },
 ];
 

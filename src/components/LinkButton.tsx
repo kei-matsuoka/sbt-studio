40a0647c9b4d8ac0cf7bb9c411ad47114deebdf0
kei-matsuoka <keimatsuoka0311@gmail.com>
2024-01-03 00:18:27 +0900

    スタイルの修正

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -10,7 +10,7 @@ export default function LinkButton({
   return (
     <Link
       href={href}
-      className="flex text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      className="flex text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700"
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"

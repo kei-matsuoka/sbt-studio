5556e3b3eca1d71dc203d54612f67a74dedc5b83
kei-matsuoka <keimatsuoka0311@gmail.com>
2024-01-04 00:28:01 +0900

    sidebarの幅修正

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,6 +1,6 @@
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="p-4 sm:ml-64">
+    <div className="p-4 sm:ml-52">
       <div className="p-4 mt-14">{children}</div>
     </div>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import { pageConfig } from '@/constants';
 
 export default function Sidebar() {
   return (
-    <nav className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white  shadow-md sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700">
+    <nav className="fixed top-0 left-0 z-40 w-52 h-screen pt-20 transition-transform -translate-x-full bg-white  shadow-md sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700">
       <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
         <ul className="space-y-2 font-medium">
           {pageConfig.map((page) => (
